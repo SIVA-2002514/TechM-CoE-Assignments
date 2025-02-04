0102b4f31a0fb7c5591a9e957047bef92b916ae9
Siva M <shivasiva2002514@gmail.com>
2025-02-04 21:32:08 +0530

    Create AnimalSound.java

---
+++ Week 1-tasks/AnimalSound.java
@@ -0,0 +1,10 @@
+public class AnimalSoundDemo {
+
+	public static void main(String[] args) {
+		// Creating an instance of the Dog class
+		Animal myDog = new Dog();
+
+		// Calling the overridden makeSound() method
+		myDog.makeSound();
+	}
+}
