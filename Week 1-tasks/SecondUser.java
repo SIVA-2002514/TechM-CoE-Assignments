e17fd54eb053be122b08f57f53653a6674c82d27
Siva M <shivasiva2002514@gmail.com>
2025-02-04 21:53:24 +0530

    Create SecondUser.java

---
+++ Week 1-tasks/SecondUser.java
@@ -0,0 +1,27 @@
+public class SecondUser {
+	private String name;
+	private int age;
+
+	// All-arguments constructor
+	public SecondUser(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	// Getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
