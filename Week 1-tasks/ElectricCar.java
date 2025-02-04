a47f955e836c07ca10973ae92630cd31fedadf20
Siva M <shivasiva2002514@gmail.com>
2025-02-04 20:20:43 +0530

    Create ElectricCar.java

---
+++ Week 1-tasks/ElectricCar.java
@@ -0,0 +1,30 @@
+//Subclass ElectricCar extends Car
+class ElectricCar extends Car {
+	private int batteryRange;
+
+	// Constructor
+	public ElectricCar(String make, String model, int year, int batteryRange) {
+		super(make, model, year);
+		this.batteryRange = batteryRange;
+	}
+
+	// Getter and Setter for battery range
+	public int getBatteryRange() {
+		return batteryRange;
+	}
+
+	public void setBatteryRange(int batteryRange) {
+		this.batteryRange = batteryRange;
+	}
+
+	// Method to charge the battery
+	public void chargeBattery() {
+		System.out.println(DisplayingValues.BATTERY_CHARGING);
+	}
+
+	// Override startEngine method to show different behavior for ElectricCar
+	@Override
+	public void startEngine() {
+		System.out.println(DisplayingValues.OVERRIDE_METHOD);
+	}
+}
