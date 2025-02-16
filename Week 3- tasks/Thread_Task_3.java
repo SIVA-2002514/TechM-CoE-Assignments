54cf4df5b327b80896bc897df093dfde92d0dad8
Siva M <shivasiva2002514@gmail.com>
2025-02-16 11:21:22 +0530

    Create Thread_Task_3.java

---
+++ Week 3- tasks/Thread_Task_3.java
@@ -0,0 +1,26 @@
+package siva;
+
+import java.util.Arrays;
+
+class SortThread extends Thread {
+	private int[] arr;
+
+	public SortThread(int[] arr) {
+		this.arr = arr;
+	}
+
+	public void run() {
+		Arrays.sort(arr);
+		System.out.println("Sorted Array: " + Arrays.toString(arr));
+	}
+}
+
+public class MultiThreadSort {
+	public static void main(String[] args) {
+		int[] numbers = { 5, 3, 8, 1, 9, 2 };
+
+		SortThread sortThread = new SortThread(numbers);
+		sortThread.start();
+	}
+}
+
