2391be225d59600b2201381ff3bd23ebe48b58e4
Siva M <shivasiva2002514@gmail.com>
2025-02-16 11:49:35 +0530

    Create CollectionAPI_Task_1.java

---
+++ Week 3- tasks/CollectionAPI_Task_1.java
@@ -0,0 +1,16 @@
+package siva;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorList {
+	public static void main(String[] args) {
+		List<String> colors = new ArrayList<>();
+		colors.add("Red");
+		colors.add("Blue");
+		colors.add("Green");
+		colors.add("Yellow");
+
+		System.out.println("Color List: " + colors);
+	}
+}
