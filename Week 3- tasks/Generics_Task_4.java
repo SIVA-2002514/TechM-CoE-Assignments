1199f3dd44ec28286f6fc54bbf986dafd7aad2c0
Siva M <shivasiva2002514@gmail.com>
2025-02-16 11:44:33 +0530

    Create Generics_Task_4.java

---
+++ Week 3- tasks/Generics_Task_4.java
@@ -0,0 +1,18 @@
+package siva;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GenericReverse {
+	public static <T> List<T> reverseList(List<T> list) {
+		List<T> reversed = new ArrayList<>(list);
+		Collections.reverse(reversed);
+		return reversed;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+		System.out.println("Reversed List: " + reverseList(numbers)); // Output: [5, 4, 3, 2, 1]
+	}
+}
