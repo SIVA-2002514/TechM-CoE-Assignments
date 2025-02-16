8adabaa5fe656aa88e73890a8fac2e25091cd548
Siva M <shivasiva2002514@gmail.com>
2025-02-16 11:39:31 +0530

    Update Generics_Task_3.java

--- Week 3- tasks/Generics_Task_3.java
+++ Week 3- tasks/Generics_Task_3.java
@@ -1,1 +1,15 @@
+package siva;
 
+import java.util.List;
+
+public class GenericSearch {
+	public static <T> int findFirstOccurrence(List<T> list, T target) {
+		return list.indexOf(target); // Returns index, -1 if not found
+	}
+
+	public static void main(String[] args) {
+		List<String> words = List.of("apple", "banana", "cherry", "date");
+		System.out.println("Index of 'cherry': " + findFirstOccurrence(words, "cherry")); // Output: 2
+		System.out.println("Index of 'grape': " + findFirstOccurrence(words, "grape")); // Output: -1
+	}
+}
