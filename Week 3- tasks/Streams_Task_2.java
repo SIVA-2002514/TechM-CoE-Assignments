f453e59d93081600186eb1e5932a67f57d991c56
Siva M <shivasiva2002514@gmail.com>
2025-02-16 12:49:02 +0530

    Create Streams_Task_2.java

---
+++ Week 3- tasks/Streams_Task_2.java
@@ -0,0 +1,21 @@
+package siva;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamCaseConversion {
+    public static void main(String[] args) {
+        List<String> words = List.of("apple", "banana", "cherry");
+
+        List<String> uppercaseWords = words.stream()
+                                           .map(String::toUpperCase)
+                                           .collect(Collectors.toList());
+
+        List<String> lowercaseWords = words.stream()
+                                           .map(String::toLowerCase)
+                                           .collect(Collectors.toList());
+
+        System.out.println("Uppercase: " + uppercaseWords);
+        System.out.println("Lowercase: " + lowercaseWords);
+    }
+}
